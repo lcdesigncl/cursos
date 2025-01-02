600e4fa0ed975009105997b13f4bf6604535f92d
lcdesign <lcdiseno@yahoo.es>
2025-01-02 20:46:06 -0300

    componentes

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,35 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div id="app" class="bg-light">
+    <!-- Aquí insertaremos el NavBar más adelante -->
+    <NavBar />
+    <header>
+      <h1 class="text-center p-4 text-primary">Bienvenido a Cursos AlfaWeb</h1>
+    </header>
+
+    <!-- Contenido principal, donde podrían ir las vistas o componentes -->
+    <main class="container mt-4">
+      <!-- Ejemplo de contenido provisional -->
+      <p>Contenido inicial de la aplicación</p>
+    </main>
+
+    <!-- Aquí insertaremos el Footer más adelante -->
+     <Footer />
+  </div>
 </template>
 
+<script>
+import NavBar from './components/AppNavBar.vue';
+import Footer from './components/AppFooter.vue';
+
+export default {
+  name: 'App',
+  components: {
+    NavBar, 
+    Footer
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+/* Opcional: Estilos locales o globales */
 </style>
